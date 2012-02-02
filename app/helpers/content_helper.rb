591ae3b2926f87cddd115ac86dda74d9829db443
Elias Haase <haase@binaerwelt.com>
2012-02-02 14:58:29 +0000

    Tweaking the style and eliminating tables

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -3,23 +3,23 @@ module ContentHelper
 		output = '<div class="post">'
 		if user_signed_in?
 			if current_user.voted?(post)
-				output += "<table><tr><td class='voted'>"
+				output += "<div class='voted arrow' id='posts_"+post.id.to_s+"'>"
 			else
-				output += "<table><tr><td class='vote' id='posts_"+post.id.to_s+"'>"
+				output += "<div class='vote arrow' id='posts_"+post.id.to_s+"'>"
 			end
 			output += link_to "&#9650;".html_safe, "#", :onClick => "vote_up("+post.id.to_s+", 'posts')"
 		else
-			output += "<table><tr><td class='vote'>"
+			output += "<div class='vote arrow'>"
 			output += link_to "&#9650;".html_safe, new_user_session_path
 		end
-		output += "<br /><br /></td><td>"
+		output += "</div><div class='post_preview_body'>"
 		output += "<div class='post_title'>"
 		(post.link.nil? or post.link.empty?) ? (output += link_to post.title, post) : (output += link_to post.title, post.link)
 		output += "<span class='host'>"+link_to("("+URI.parse(post.link).host.gsub("www.","")+")", "http://"+URI.parse(post.link).host)+"</span>" unless post.link.nil? or post.link.empty?
 		output += "</div>"
 		output += "<div class='infobar'>"+pluralize(post.votes, "point") + " by " + post.user.name + " "+time_ago_in_words(post.created_at) + " ago | " + link_to(pluralize(comment_count(post), "comment"), post)
 		output += "</div>"
-		output += "</td></tr></table>"
+		output += "</div>"
 		output += "</div>"
 		output.html_safe
 	end
@@ -30,16 +30,16 @@ module ContentHelper
 			output += '<div class="comment">'
 			if user_signed_in?
 				if current_user.voted?(comment)
-					output += "<table><tr><td class='voted' id='comments_"+comment.id.to_s+"'>"
+					output += "<div class='voted arrow' id='comments_"+comment.id.to_s+"'>"
 				else
-					output += "<table><tr><td class='vote' id='comments_"+comment.id.to_s+"'>"
+					output += "<div class='vote arrow' id='comments_"+comment.id.to_s+"'>"
 				end
 				output += link_to "&#9650;".html_safe, "#", :onClick => "vote_up("+comment.id.to_s+", 'comments')"
 			else
-				output += "<table><tr><td class='vote' id='comments_"+comment.id.to_s+"'>"
+				output += "<div class='vote arrow'>"
 				output += link_to "&#9650;".html_safe, new_user_session_path
 			end
-			output += "<br /><br /></td><td>"
+			output += "</div><div class='text_body'>"
 			output += simple_format(auto_link(comment.text))
 			output += "<div class='infobar'>"+pluralize(comment.votes, "point") + " by " + comment.user.name + " "+time_ago_in_words(comment.created_at) + " ago &nbsp;"
 			if can? :update, comment
@@ -48,7 +48,7 @@ module ContentHelper
 			output += "</div>"
 			output += link_to 'Comment', "#", "onClick" => "$('#comment_form_#{comment.id.to_s}').slideToggle()" if can? :create, Comment
 			output += "<br />"
-			output += "</td></tr></table>"
+			output += "</div>"
 			output += "<div id='comment_form_#{comment.id.to_s}' style='display: none'>"
 			output += render :partial => 'comments/form', :locals => { :commentable_type => "Comment", :commentable_id => comment.id } 
 			output += "</div>"
@@ -63,16 +63,16 @@ module ContentHelper
 		output = '<div class="post">'
 		if user_signed_in?
 			if current_user.voted?(post)
-				output += "<table><tr><td class='voted' id='posts_"+post.id.to_s+"'>"
+				output += "<div class='voted arrow' id='posts_"+post.id.to_s+"'>"
 			else
-				output += "<table><tr><td class='vote' id='posts_"+post.id.to_s+"'>"
+				output += "<div class='vote arrow' id='posts_"+post.id.to_s+"'>"
 			end
 			output += link_to "&#9650;".html_safe, "#", :onClick => "vote_up("+post.id.to_s+", 'posts')"
 		else
-			output += "<table><tr><td class='vote'>"
+			output += "<div class='vote arrow'>"
 			output += link_to "&#9650;".html_safe, new_user_session_path
 		end
-		output += "<br /><br /></td><td>"
+		output += "</div><div class='text_body'>"
 		(post.link.nil? or post.link.empty?) ? (output += "<strong>"+link_to(post.title, post)+"</strong>") : (output += "<strong>"+link_to(post.title, post.link)+"</strong>")
 		output += "<span class='host'>"+link_to("("+URI.parse(post.link).host.gsub("www.","")+")", "http://"+URI.parse(post.link).host)+"</span>" unless post.link.nil? or post.link.empty?
 		output += simple_format(auto_link(post.text))
@@ -89,7 +89,7 @@ module ContentHelper
 		output += '<div id="comment_form" style="display: none; clear: left">'
 		output += render :partial => 'comments/form', :locals => { :commentable_type => "Post", :commentable_id => post.id }
 		output += "</div>"
-		output += '</td></tr></table></div>'
+		output += '</div></div>'
 		output
 	end
 end
