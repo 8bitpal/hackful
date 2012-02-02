590828938d0c5a735037acd66761d5d616cee59d
Elias Haase <haase@binaerwelt.com>
2012-02-02 23:10:21 +0000

    Added page navigation & style tweaks

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,10 +1,12 @@
 class ContentController < ApplicationController
   def frontpage
-		@posts = Post.find_by_sql("SELECT * FROM posts ORDER BY ((posts.up_votes - posts.down_votes) -1 )/POW((((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(posts.created_at)) / 3600 )+2), 1.5) DESC LIMIT 15")
+		params[:page].nil? ? @page = 0 : @page = params[:page].to_i
+		@posts = Post.find_by_sql ["SELECT * FROM posts ORDER BY ((posts.up_votes - posts.down_votes) -1 )/POW((((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(posts.created_at)) / 3600 )+2), 1.5) DESC LIMIT ?, 15", (@page*15)]
   end
 
   def new
-		@posts = Post.find(:all, :order => "created_at DESC", :limit => 15)
+		@page = params[:page].to_i
+		@posts = Post.find(:all, :order => "created_at DESC", :limit => ["?,15", @wpage*15])
   end
 
 end
