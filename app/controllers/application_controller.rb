469d0f92ededb5e1fb24c59ca5328a22ae7986f2
Elias Haase <haase@binaerwelt.com>
2012-02-03 20:10:36 +0000

    Setting meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :meta_defaults
+
+  def meta_defaults
+    @meta_keywords = "Europe, Startups, Entrepreneurs"
+    @meta_description = "Hackful Europe is a place for European entrepreneurs to share demos, stories or ask questions."
+  end
 end
