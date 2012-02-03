04adbf25488a517b75b9b7ca82bb13b4763ccfe1
Elias Haase <haase@binaerwelt.com>
2012-02-03 15:40:48 +0000

    Implemented RSS for /new and /frontpage

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -2,11 +2,21 @@ class ContentController < ApplicationController
   def frontpage
 		params[:page].nil? ? @page = 0 : @page = params[:page].to_i
 		@posts = Post.find_by_sql ["SELECT * FROM posts ORDER BY ((posts.up_votes - posts.down_votes) -1 )/POW((((UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(posts.created_at)) / 3600 )+2), 1.5) DESC LIMIT ?, 15", (@page*15)]
+		
+		respond_to do |f|
+			f.html
+			f.rss { render :layout => false }
+		end
   end
 
   def new
 		params[:page].nil? ? @page = 0 : @page = params[:page].to_i
 		@posts = Post.find(:all, :order => "created_at DESC", :limit => 15, :offset => (@page*15))
+		
+		respond_to do |f|
+			f.html
+			f.rss { render :layout => false }
+		end
   end
   
   def about
