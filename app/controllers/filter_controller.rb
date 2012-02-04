1da002bccec74345ff275a093ec4b5d4f9c381e8
Elias Haase <haase@binaerwelt.com>
2012-02-04 08:52:18 +0000

    Code cleanup

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -1,9 +1,9 @@
 class FilterController < ApplicationController
 	check_authorization
-  load_and_authorize_resource
+	load_and_authorize_resource
 	
 	rescue_from CanCan::AccessDenied do |exception|
-    flash[:error] = exception.message
-    redirect_to new_user_session_path
-  end
+		flash[:error] = exception.message
+		redirect_to new_user_session_path
+	end
 end
