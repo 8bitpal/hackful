2c575f85ced4343cb5a0928529f7135ff4f1ea9a
Elias Haase <haase@binaerwelt.com>
2012-02-09 13:13:02 +0000

    Implement autolinking again, fixes #19

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,6 @@ module ApplicationHelper
 		require 'rdiscount'
 
 		text = sanitize(text) unless text.html_safe? || options.delete(:safe)
-		(text.blank? ? "" : RDiscount.new(text, :filter_html ).to_html.gsub(/(?<!\w)(@(\w+))/, '<a href="http://twitter.com/\2">\1</a>').html_safe)
+		(text.blank? ? "" : RDiscount.new(text, :filter_html, :autolink ).to_html.gsub(/(?<!\w)(@(\w+))/, '<a href="http://twitter.com/\2">\1</a>').html_safe)
 	end
 end
