f922849829cb287a5cdb7cc640e3e9eeef87258e
Damian Serrano Thode <dsthode@gmail.com>
2012-02-29 00:44:05 +0100

    Added code for hackful issue request #16

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -4,6 +4,7 @@ class FilterController < ApplicationController
 	
 	rescue_from CanCan::AccessDenied do |exception|
 		flash[:error] = exception.message
+		session[:user_return_to] = request.url
 		redirect_to new_user_session_path
 	end
 end
