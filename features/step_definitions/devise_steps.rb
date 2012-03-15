853add9a5a96535b610cd8b328d355f3e0404883
Patrik Björklund <p.bjorklund@gmail.com>
2012-03-15 14:34:06 +0100

    Posts feature, factory, shared_steps created
    Feature for submitting, editing. Scenario for status change when user
    deletes account.
    
    Move steps out into shared_steps that overlapp several features

--- features/step_definitions/devise_steps.rb
+++ features/step_definitions/devise_steps.rb
@@ -1,26 +1,3 @@
-Given /^I am on the startpage$/ do
-  visit "/"
-end
-
-Given /^I am a new authenticated user$/ do
-  email = 'testing@man.net'
-  password = 'secretpass'
-  FactoryGirl.create(:user, email: email, password: password, password_confirmation: password)
-
-  visit '/users/sign_in'
-  fill_in "user_email", :with=>email
-  fill_in "user_password", :with=>password
-  click_button "Sign in"
-end
-
-Given /^I am not authenticated$/ do
-  visit('/users/sign_out') 
-end
-
-When /^I click "([^"]*)"$/ do |link|
-  click_link link
-end
-
 When /^I fill in new user details$/ do
   within(".body") do
     fill_in 'User Name', with: 'pbjorklund'
@@ -29,10 +6,6 @@ When /^I fill in new user details$/ do
     fill_in 'Password confirmation', :with => 'password'
   end
   click_button "Sign up"
-end
-
-Then /^I should see "([^"]*)"$/ do |text|
-  page.should have_content(text)
 end
 
 When /^I fill in my user details$/ do
